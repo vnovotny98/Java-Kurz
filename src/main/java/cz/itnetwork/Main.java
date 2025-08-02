d012601aa759d92a045840389c56bcc8bb61afe7
vnovotny98 <novotavojta@gmail.com>
2025-08-02 11:54:26 +0200

    First commit - session 01-07

---
+++ src/main/java/cz/itnetwork/Main.java
@@ -0,0 +1,96 @@
+package cz.itnetwork;
+
+//import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Vítejte v kalkulačce");
+        System.out.println("Zadejte první číslo:");
+        double a = Double.parseDouble(scanner.nextLine());
+        System.out.println("Zadejte druhé číslo:");
+        double b = Double.parseDouble(scanner.nextLine());
+        double soucet = a + b;
+        double rozdil = a - b;
+        double soucin = a * b;
+        double podil = a / b;
+        System.out.println("Součet: " + soucet);
+        System.out.println("Rozdíl: " + rozdil);
+        System.out.println("Součin: " + soucin);
+        System.out.println("Podíl: " + podil);
+        System.out.println("Děkuji za použití kalkulačky");
+        */
+
+        /*
+        boolean nepravda = false;
+        boolean vyraz = (15 > 5);
+        System.out.println(nepravda);
+        System.out.println(vyraz);
+        */
+
+        /*
+        String vstup = "Krokonoshroch";
+        System.out.println(vstup.startsWith("krok"));
+        System.out.println(vstup.endsWith("hroch"));
+        System.out.println(vstup.contains("nos"));
+        System.out.println(vstup.contains("roh"));
+        */
+
+        /*
+        String mezera= " ";
+        String prazdno = "";
+        String retezec = "Retezec";
+        System.out.println(mezera.isEmpty());
+        System.out.println(prazdno.isEmpty());
+        System.out.println(retezec.isEmpty());
+         */
+
+        /*
+        String nastaveni = "Fullscreen shaDows autosave";
+        nastaveni = nastaveni.toLowerCase();
+        System.out.println(nastaveni.contains("fullscreen"));
+        */
+
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Zadejte číslo:");
+        String vstup = scanner.nextLine();
+        System.out.println("Zadal jste číslo: " + vstup);
+        System.out.println("Text po funkci trim: " + vstup.trim());
+        int cislo = Integer.parseInt(vstup.trim());
+        System.out.println("Převedel jsem zadaný text na číslo parsováním, zadal jste: " + cislo);
+        */
+
+        /*
+        String veta = "C# je nejlepší!";
+        veta = veta.replace("C#", "Java");
+        System.out.println(veta);
+         */
+
+        /*
+        int a = 10;
+        int b = 20;
+        int soucet = a + b;
+        String veta = String.format("Když sečteme %d a %d, dostaneme %d", a, b, soucet);
+        System.out.println(veta);
+         */
+
+        /*
+        int a = 10;
+        int b = 20;
+        int soucet = a + b;
+        System.out.printf("Když sečteme %d a %d, dostaneme %d", a, b, soucet);
+        */
+
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Zadejte vaše jméno:");
+        String jmeno = scanner.nextLine();
+        System.out.printf("Délka vašeho jména je: %d", jmeno.length());
+         */
+
+
+    }
+}
