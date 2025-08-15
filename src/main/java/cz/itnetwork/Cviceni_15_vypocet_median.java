416d825199267858822f06ae95f9cfbf3e1d1750
vnovotny98 <novotavojta@gmail.com>
2025-08-15 08:10:16 +0200

    Practice 15

--- src/main/java/cz/itnetwork/Cviceni_15_vypocet_median.java
+++ src/main/java/cz/itnetwork/Cviceni_15_vypocet_median.java
@@ -10,6 +10,18 @@ public class Cviceni_15_vypocet_median {
 
         System.out.println("Zadej počet čísel: ");
         int pocet = Integer.parseInt(scanner.nextLine());
+        int[] zadanecisla = new int[pocet];
+        for (int i=0; i < pocet; i++){
+            System.out.printf("Zadej %d. číslo: \n", i + 1);
+            zadanecisla[i] = Integer.parseInt(scanner.nextLine());
+        }
 
+        int[] serazenacisla = zadanecisla.clone();
+        Arrays.sort(serazenacisla);
+        double median = serazenacisla[pocet/2];
+
+        for (int i=0; i < pocet; i++){
+            System.out.printf("%d se od mediánu odchyluje o %f \n", zadanecisla[i],zadanecisla[i]-median);
+        }
     }
 }
